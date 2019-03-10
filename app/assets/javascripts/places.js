a4b2c01763adcd5b32c71a744b46456f4588516f
Dmitriy Shamritskiy <shamritskiy3468@gmail.com>
2019-03-10 23:45:10 +0300

    Pre-Merge commit

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,6 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function initMap(lat, lng) {
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+    center: myCoords,
+    zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
 function initMap2() {
     var lat = document.getElementById('place_latitude').value;
     var lng = document.getElementById('place_longitude').value;
