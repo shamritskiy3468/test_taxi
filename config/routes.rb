d457ac633eddf5c8428e13acd9ad2410bb8c942a
Dmitriy Shamritskiy <shamritskiy3468@gmail.com>
2019-04-16 01:52:28 +0300

    Continue working on html and devise in controller branch...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
+  #devise_for :users, controllers: { registrations: 'users/registrations' }
   #devise_for :users
   devise_for :admin_users
 
